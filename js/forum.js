ece26a4ca423de8b7ff9280cf6c986d32ff11952
chaerin918 <s2018s34@e-mirim.hs.kr>
2020-06-07 23:14:03 +0900

    이미지 드래그 금지하기

---
+++ js/forum.js
@@ -0,0 +1,4 @@
+document.getElementById('cloud1').setAttribute('draggable', false);
+document.getElementById('cloud2').setAttribute('draggable', false);
+document.getElementById('titleImage').setAttribute('draggable', false);
+
